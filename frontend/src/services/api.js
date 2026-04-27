0570bc07f6ff6126c2fffc1153664074d7b9e7c7
naveenchahal <n6179323@gmail.com>
2026-04-27 06:33:50 +0530

    fix: update API baseURL to use environment variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 })
 
 api.interceptors.request.use(config => {
